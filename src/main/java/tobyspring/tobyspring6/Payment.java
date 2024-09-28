58f58ef09e277beaed08d926b954220779954bb1
Do-oya <qkrwjdgus1441@naver.com>
2024-09-28 22:46:44 +0900

    PaymentService 개발 (2)
    - 영상 실습과는 다르게 lombok builder 사용

--- src/main/java/tobyspring/tobyspring6/Payment.java
+++ src/main/java/tobyspring/tobyspring6/Payment.java
@@ -1,4 +1,20 @@
 package tobyspring.tobyspring6;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@ToString
 public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
 }
--- src/main/java/tobyspring/tobyspring6/PaymentService.java
+++ src/main/java/tobyspring/tobyspring6/PaymentService.java
@@ -1,13 +1,24 @@
 package tobyspring.tobyspring6;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare() {
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+
+        return Payment.builder()
+                .orderId(orderId)
+                .currency(currency)
+                .foreignCurrencyAmount(foreignCurrencyAmount)
+                .exRate(BigDecimal.ZERO)
+                .convertedAmount(BigDecimal.ZERO)
+                .validUntil(LocalDateTime.now())
+                .build();
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
 }
